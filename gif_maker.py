f3416a52414dbf66c9456e3b7bbe8ca2515f4a37
devanomaly <rafaelfpcosta@gmail.com>
2021-10-24 17:46:42 -0300

    voltando gif_maker... nao refatorei tudo mas ok

--- gif_maker.py
+++ gif_maker.py
@@ -6,7 +6,7 @@ def get_key(fp):
     numeric = filename.split("=")[-1]
     return int(numeric)
   
-def make_gif(file_name):
+def make_gif(frame_folder, file_name):
     resultados_glob = sorted(glob.glob("*.png"), key=get_key)
     for resultado in resultados_glob:
         print(resultado)
@@ -14,3 +14,6 @@ def make_gif(file_name):
     frame_one = frames[0]
     print(len(frames))
     frame_one.save(str(file_name)+".gif", format="GIF", append_images=frames,save_all=True, duration=50, loop=0)
+
+if __name__ == "__main__":
+    make_gif("/path/to/images", "babinet?")
